a43014a1d6932f935556be4a98d105db96778a77
Hosain <hosainandh@gmail.com>
2025-09-05 01:02:37 +0600

    Fix CORS issue for socket.io and add sample env

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -9,7 +9,7 @@ const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: [process.env.FRONTEND_URL || "http://localhost:5173"],
+    origin: [process.env.CLIENT_URL || "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   },
